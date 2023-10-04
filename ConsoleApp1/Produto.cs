9fa7a9825173e0e6aa3bea971988ed142281293a
ISABELLE TSCHOEKE VOLACO <isabelletvolaco@gmail.com>
2023-10-03 21:14:41 -0300

    adicionei classe produto contendo batatinhas

---
+++ ConsoleApp1/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objetosorientados
+{
+    internal class Produto
+    {
+     
+        static void ImprimirBatatas()
+        {
+            Console.WriteLine("oi batatinha");
+        }
+
+    }
+}
+
